71147d29e5ddd3f8a762c9119903a8aca5a738a7
NinoosMoshi <nalkaton@gmail.com>
2022-11-06 17:00:18 -0500

    angular login and registration component  #14

--- frontEnd/user-admin-app/src/app/app.module.ts
+++ frontEnd/user-admin-app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoginComponent } from './components/security/login/login.component';
+import { RegisterComponent } from './components/security/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
