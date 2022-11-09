6a3b43e339ede25d236ae5923ce0a91b4475c5fd
NinoosMoshi <nalkaton@gmail.com>
2022-11-09 08:31:54 -0500

    angular executeAuthentication method #18

---
+++ frontEnd/user-admin-app/src/app/services/security/authentication.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  baseUrl = 'http://localhost:8080';
+
+  constructor(private http:HttpClient) { }
+
+
+    // http://localhost:8080/login
+    executeAuthentication(email:string, password:string):Observable<any>{
+      return this.http.post<any>(`${this.baseUrl}/login`,{email,password}).pipe(
+        map(response =>{
+          return response
+        })
+      );
+    }
+
+
+
+
+
+
+}
